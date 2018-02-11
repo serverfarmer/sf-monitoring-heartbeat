5f9cdb56533503add2be139917c401f95f55b0e2
Tomasz Klim <github@tomaszklim.pl>
2018-02-11 13:20:53 +0100

    support for qnap devices

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -x /opt/bin/nc ] && [ ! -x /bin/nc ]; then
+	ln -s /opt/bin/nc /bin/nc
+fi
+
 if ! grep -q /opt/farm/ext/monitoring-heartbeat/cron/update.sh /etc/crontab; then
 	echo "setting up crontab entry"
 	echo "*/2 * * * * root /opt/farm/ext/monitoring-heartbeat/cron/update.sh" >>/etc/crontab
