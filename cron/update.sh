e3dce5beb5ffcb71b2503501dcf6f12319d3b8ba
Tomasz Klim <github@tomaszklim.pl>
2018-02-22 15:10:12 +0100

    new checks

--- cron/update.sh
+++ cron/update.sh
@@ -23,6 +23,8 @@ check_service 9300 elasticcluster
 
 check_service 80 http
 check_service 443 https
+check_service 3128 squid
+check_service 3000 app3000
 check_service 8080 app8080
 
 check_service 25 smtp
@@ -31,11 +33,20 @@ check_service 993 imaps
 check_service 110 pop3
 check_service 995 pop3s
 
+check_service 137 netbiosns
+check_service 138 netbiosdgm
+check_service 139 netbiosssn
+check_service 445 samba
+
 check_service 21 ftp
 check_service 22 ssh
+check_service 23 telnet
 check_service 53 dns
 check_service 111 portmap
+check_service 389 ldap
+check_service 631 ipp
 check_service 953 rdnc
+check_service 1194 openvpn
 check_service 2812 monit
 check_service 3260 iscsi
 check_service 11211 memcached
