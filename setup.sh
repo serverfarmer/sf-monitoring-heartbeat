647eb74a5c4294dde4d7879aec213ef4bc10fbe6
Tomasz Klim <github@tomaszklim.pl>
2018-10-18 22:20:01 +0200

    move also detected lists of directories/drives to heartbeat configuration

--- setup.sh
+++ setup.sh
@@ -18,10 +18,12 @@ if [ "$HWTYPE" != "container" ] && [ "$HWTYPE" != "lxc" ]; then
 	ln -sf /opt/farm/ext/monitoring-heartbeat/hooks/smart.sh /etc/heartbeat/hooks/smart.sh
 fi
 
-conditional_move /etc/local/.config/skip-smart.devices /etc/heartbeat/skip-smart.sata
-conditional_move /etc/local/.config/skip-smart.raid    /etc/heartbeat/skip-smart.raid
-conditional_move /etc/local/.config/allowed.smart      /etc/heartbeat/known-smart-defects.conf
-conditional_move /etc/local/.config/heartbeat.url      /etc/heartbeat/server.url
+conditional_move /etc/local/.config/skip-smart.devices      /etc/heartbeat/skip-smart.sata
+conditional_move /etc/local/.config/skip-smart.raid         /etc/heartbeat/skip-smart.raid
+conditional_move /etc/local/.config/allowed.smart           /etc/heartbeat/known-smart-defects.conf
+conditional_move /etc/local/.config/heartbeat.url           /etc/heartbeat/server.url
+conditional_move /etc/local/.config/raid.drives             /etc/heartbeat/detected-raid-drives.conf
+conditional_move /etc/local/.config/space-check.directories /etc/heartbeat/detected-data-directories.conf
 
 /opt/farm/ext/packages/utils/install.sh sudo smartmontools
 
