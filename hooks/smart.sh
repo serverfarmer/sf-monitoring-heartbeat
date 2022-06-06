54861f93d36479340719a87205b39b9a06b5b1e5
Tomasz Klim <github@tomaszklim.pl>
2022-06-06 13:03:04 +0200

    removed new relic support and old transitional code

--- hooks/smart.sh
+++ hooks/smart.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
 file=$1
-base=`basename $file .txt`
-
-type=`echo $base |cut -d- -f1`
-device=`echo $base |cut -d- -f2-`
-
-if [ "$type" = "ata" ] && [ -s /etc/local/.config/newrelic.license ]; then
-	/opt/farm/ext/monitoring-heartbeat/targets/sata-newrelic.sh $device $file
-fi
 
 if [ -d /opt/farm/ext/monitoring-cacti ]; then
 	/opt/farm/ext/monitoring-cacti/cron/send.sh $file
--- setup.sh
+++ setup.sh
@@ -2,28 +2,11 @@
 . /opt/farm/scripts/init
 
 
-conditional_move() {
-	oldfile=$1
-	newfile=$2
-
-	if [ -f $oldfile ] && [ ! -f $newfile ]; then
-		mv $oldfile $newfile
-	fi
-}
-
-
 mkdir -p /etc/heartbeat/hooks
 
 if [ "$HWTYPE" != "container" ] && [ "$HWTYPE" != "lxc" ]; then
 	ln -sf /opt/farm/ext/monitoring-heartbeat/hooks/smart.sh /etc/heartbeat/hooks/smart.sh
 fi
-
-conditional_move /etc/local/.config/skip-smart.devices      /etc/heartbeat/skip-smart.sata
-conditional_move /etc/local/.config/skip-smart.raid         /etc/heartbeat/skip-smart.raid
-conditional_move /etc/local/.config/allowed.smart           /etc/heartbeat/known-smart-defects.conf
-conditional_move /etc/local/.config/heartbeat.url           /etc/heartbeat/server.url
-conditional_move /etc/local/.config/raid.drives             /etc/heartbeat/detected-raid-drives.conf
-conditional_move /etc/local/.config/space-check.directories /etc/heartbeat/detected-data-directories.conf
 
 /opt/farm/scripts/setup/extension.sh sf-db-utils
 /opt/farm/scripts/setup/extension.sh heartbeat-linux
