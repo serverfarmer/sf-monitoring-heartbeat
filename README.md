6d8025a7b0a690dd3394e49c72018152d347ab1d
Tomasz Klim <github@tomaszklim.pl>
2022-06-06 13:16:23 +0200

    updated readme, removed more transitional code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This extension has been rewritten as Heartbeat subproject. See https://github.com/serverfarmer/heartbeat-linux for details.
+## Overview
+
+`sf-monitoring-heartbeat` Server Farmer extension is responsible for:
+
+- installing and configuring Server Farmer Heartbeat (see https://github.com/serverfarmer/heartbeat-linux and https://github.com/serverfarmer/heartbeat-server for details)
+- enabling SMART reporting to Cacti through [`sf-monitoring-cacti`](https://github.com/serverfarmer/sf-monitoring-cacti) (if it is also installed)
--- setup.sh
+++ setup.sh
@@ -10,15 +10,3 @@ fi
 
 /opt/farm/scripts/setup/extension.sh sf-db-utils
 /opt/farm/scripts/setup/extension.sh heartbeat-linux
-
-if grep -q /opt/farm/ext/monitoring-heartbeat/cron /etc/crontab; then
-	sed -i -e "/\/opt\/farm\/ext\/monitoring-heartbeat\/cron/d" /etc/crontab
-fi
-
-if grep -q /opt/farm/ext/monitoring-smart/cron /etc/crontab; then
-	sed -i -e "/\/opt\/farm\/ext\/monitoring-smart\/cron/d" /etc/crontab
-fi
-
-if grep -q /opt/farm/ext/monitoring-space/cron /etc/crontab; then
-	sed -i -e "/\/opt\/farm\/ext\/monitoring-space\/cron/d" /etc/crontab
-fi
